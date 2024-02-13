075a61cf50c38a287e164dcc218204daf860be0b
= <=>
2024-02-13 13:24:14 +0800

    added props

--- frontend/src/components/ProductsCard.vue
+++ frontend/src/components/ProductsCard.vue
@@ -1,7 +1,76 @@
+<style >
+.my-card
+{
+  max-width: 230px;
+  height: 100px;
+}
+</style>
+
+<script setup>
+
+const props = defineProps(
+  {
+    name:
+    {
+      required: true,
+      type: String
+    },
+    rating:
+    {
+      required: true,
+      type: Number
+    },
+    price:
+    {
+      required: true,
+      type: String
+    }
+
+  })
+
+
+</script>
+
+
 <template>
 
+<q-card class="my-card" flat bordered>
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
 
-<h4>my card</h4>
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
 
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            {{props.name}}
+          </div>
+
+        </div>
+
+        <q-rating v-model="ratingModel" :model-value="props.rating" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          {{ props.price  }}
+        </div>
+
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
 
 </template>
--- frontend/src/pages/ProductsCatalog.vue
+++ frontend/src/pages/ProductsCatalog.vue
@@ -3,6 +3,9 @@
 
 <h3>PRODUCT CATALOG</h3>
 
+<div class="q-pa-md">
+  <ProductCard name="French fries" rating="3" price="$6,000"/>
+</div>
 
 
 </template>
